bd07a2568994354452a792c0b9291f4fd26e8956
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2017-02-20 22:10:34 -0300

    Criacao do arquivo de contato

--- api/api.js
+++ api/api.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var app = express();
 
-app.get('/', (req, res) => {
+app.get('/', function(req, res) {
     res.json({
     	id: 1,
     	nome: 'VOU TROCAR DE NOVO'
