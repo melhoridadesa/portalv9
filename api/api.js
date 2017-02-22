fef71f9b791fc53b8ce2c6822850ecb46ea8ef1f
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2017-02-21 21:00:32 -0300

    mysql conn

--- api/api.js
+++ api/api.js
@@ -3,11 +3,31 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-    res.json({
-    	id: 1,
-    	nome: 'VOU TROCAR DE NOVO'
+
+    var mysql      = require('mysql');
+    var connection = mysql.createConnection({
+        host     : 'localhost',
+        user     : 'root',
+        password : 'mdba2007',
+        database : 'dbmelhoridade'
     });
+
+    connection.connect();
+
+    connection.query('select titulo,link,imagem from tb_banner;',
+        function (error, results, fields) {
+        if (error){
+            console.log(error);
+        }else {
+            res.json(results);
+        }
+
+    });
+
+    connection.end();
 });
+
+
 
 app.listen(4000, function() {
     console.log('http://localhost:4000');
